029a474ab861735efb99c8b3c731234ba8578055
Roman <yoyoshkalol@gmail.com>
2023-11-09 21:11:33 +0300

    wtf again

--- src/main/java/LessonTwo.java
+++ src/main/java/LessonTwo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class LessonTwo {
+public class LessonTwo {
+    public static void main(String[] args) {
+        checkRange(16,5);
+    }
+
+    public static void checkRange(int a, int b){
+        int c = a + b;
+        if(c>=10 && c<=20){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
 }
