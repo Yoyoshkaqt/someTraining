5767c169ecbded30b4b944f627ab9e4d5cc6fbcc
Roman <yoyoshkalol@gmail.com>
2023-11-09 21:48:08 +0300

    more stuff

--- src/main/java/LessonTwo.java
+++ src/main/java/LessonTwo.java
@@ -1,6 +1,9 @@
 public class LessonTwo {
     public static void main(String[] args) {
         checkRange(16,5);
+        System.out.println(checkRangeTwo(1,2));
+        positiveOrNegative(-999999);
+        System.out.println(positiveOrNegativeTwo(2));
     }
 
     public static void checkRange(int a, int b){
@@ -9,6 +12,31 @@ public class LessonTwo {
             System.out.println("true");
         }else{
             System.out.println("false");
+        }
+    }
+
+    public static boolean checkRangeTwo(int a, int b){
+        int c = a + b;
+        if(c>=10 && c<=20){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void positiveOrNegative(int a){
+        if(a>=0){
+            System.out.println("Число положительное");
+        }else{
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean positiveOrNegativeTwo(int a){
+        if(a>=0){
+            return false;
+        }else {
+            return true;
         }
     }
 }
