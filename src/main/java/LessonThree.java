563406bdc2eb044234e04a22db2040697c38fcfc
Roman <yoyoshkalol@gmail.com>
2023-11-14 21:09:27 +0300

    new pain

---
+++ src/main/java/LessonThree.java
@@ -0,0 +1,14 @@
+public class LessonThree {
+    public static void main(String[] args) {
+
+    }
+
+    public class Employee{
+        String fullName;
+        String jobTitle;
+        String email;
+        String phoneNumber;
+        int salary;
+        int age;
+    }
+}
