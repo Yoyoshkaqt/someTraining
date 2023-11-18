e07bfccd11029771bcc37f27783355369181f76b
Roman <yoyoshkalol@gmail.com>
2023-11-18 11:28:55 +0300

    Done

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -1,4 +1,35 @@
 public class Park {
+    public static void main(String[] args) {
+        Attraction attraction = new Attraction("Ракушка",100,"10-18 AM");
+    }
 
+}
+class Attraction {
+    Price prices;
+    String name;
+
+    public Attraction(String name,int price, String workingHours){
+        this.name = name;
+        Price price1 = new Price(price);
+        WorkingHours workHour = new WorkingHours(workingHours);
+
+    }
+
+    class Price{
+        int price;
+
+        public Price(int price){
+            this.price = price;
+        }
+
+    }
+
+    class WorkingHours{
+        String workingHours;
+
+        public WorkingHours(String workingHours){
+            this.workingHours = workingHours;
+        }
+    }
 
 }
